278bf88c8cef54de9eb18cbb54012d149cc1f022
nazim uddin <mdnajim1247@gmail.com>
2025-06-20 22:44:33 +0600

    created project and created navbar and home page layout stractut done

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,60 @@
+import Link from 'next/link';
+import React from 'react';
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from '../ui/navigation-menu';
+import { Switch } from '../ui/switch';
+import { Button } from '../ui/button';
+import { IoMdMenu } from 'react-icons/io';
+
+const Navbar = () => {
+    return (
+        <header className='py-4 shadow-md'>
+            <nav className='max-w-6xl mx-auto flex items-center justify-between'>
+                {/* logo */}
+                <div>
+                    <Link href={'/'}>Daily News</Link>
+                </div>
+                {/* desktop menu */}
+                <NavigationMenu className='hidden lg:flex'>
+                    <NavigationMenuList>
+                        <NavigationMenuItem className='flex items-center'>
+                            <NavigationMenuLink href='/news'>News</NavigationMenuLink>
+                            <NavigationMenuLink href='/services'>
+                            <NavigationMenuTrigger>Services</NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <ul>
+                                    <li>
+<NavigationMenuLink href='/services/web'>Web Development</NavigationMenuLink>
+                                    </li>
+                                    <li>
+<NavigationMenuLink href='/services/apps'>Mobile Apps</NavigationMenuLink>
+                                    </li>
+                                    <li>
+<NavigationMenuLink href='/services/seo'>SEO</NavigationMenuLink>
+                                    </li>
+                                </ul>    
+                            </NavigationMenuContent>
+                            </NavigationMenuLink>
+ <NavigationMenuLink href='/about'>About</NavigationMenuLink>                     
+ <NavigationMenuLink href='/contact'>Contact</NavigationMenuLink>                     
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+                {/* login button */}
+                <div className='hidden lg:flex items-center'>
+                    <div>
+                        <span>Dark Mode</span>
+                        <Switch />
+                    </div>
+                    <Button variant="outline">Button</Button>
+                </div>
+                <div className='lg:hidden'>
+                    <Button>
+                        <IoMdMenu />
+                    </Button>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
